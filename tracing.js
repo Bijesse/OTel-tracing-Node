10c6de3a76a031f1645dee802cfec4eff236abf5
Tom Bijesse <tom@bijesse.com>
2022-02-09 10:39:46 -0800

    lines 10 and 11

--- tracing.js
+++ tracing.js
@@ -3,13 +3,15 @@
 const process = require('process');
 const opentelemetry = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { OTLPTraceExporter } =  require('@opentelemetry/exporter-trace-otlp-proto');
 
-// configure the SDK to export telemetry data to the console
-// enable all auto-instrumentations from the meta package
-const traceExporter = new ConsoleSpanExporter();
+const collectorOptions = {
+};
+
+const traceExporter = new OTLPTraceExporter(collectorOptions);
+
 const sdk = new opentelemetry.NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'my-service',
